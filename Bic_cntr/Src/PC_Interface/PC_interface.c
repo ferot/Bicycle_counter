1bd4a7b322338c8deb107c9e21af689a2c87e283
ferot <feeroo@gmail.com>
2017-06-18 18:15:52 +0200

    Wrapper for logging in usb state machine

--- Bic_cntr/Src/PC_Interface/PC_interface.c
+++ Bic_cntr/Src/PC_Interface/PC_interface.c
@@ -9,6 +9,7 @@
 #include "lcd_pattern.h"
 #include "tm_stm32_hd44780.h"
 #include "usbd_def.h"
+#include "usbd_cdc_if.h"
 
 PC_Interface usbInterfaceMod = {
 		.receivedData = {0},
@@ -18,74 +19,92 @@ PC_Interface usbInterfaceMod = {
 		.runStateMachine = state_machine
 };
 
-void copy_data_to_string(p_PCIntPtr PCIptr){
+/**
+ * Wrapping function displaying string in begginning of second row
+ * @param str - string to put on display
+ */
+void lcdPutStrSecRow(char* str) {
+	TM_HD44780_Puts(0, SECOND_ROW, str);
+	Delayms(200);
+}
+
+/**
+ * Function responsible for converting data from USB buffer into string
+ */
+void copy_data_to_string(p_PCIntPtr PCIptr) {
 	for (int i = 0; i < USB_COMM_BUF_SIZE; i++)
 		PCIptr->messageBuffer[i] = (char) PCIptr->receivedData[i];
 }
 
-/*
+/**
  * Function responsible for handling usb-communication state machine
+ * @param PCIptr - interface to PC interface module
  */
 int state_machine(p_PCIntPtr PCIptr) {
 	int res = USBD_FAIL;
 	int message_length = 0;
 
-	switch(PCIptr->usbState){
+	switch (PCIptr->usbState) {
 	case USB_SUBSTATE_INIT:
-		TM_HD44780_Puts(0, SECOND_ROW, PCIptr->receivedData);
+		lcdPutStrSecRow((char*)PCIptr->receivedData);
 
-		if (strstr(PCIptr->receivedData, "hello_host")) {
+		if (strstr((char*)PCIptr->receivedData, "hello_host")) {
 			PCIptr->usbState = USB_SUBSTATE_ACK;
 			return USB_SUBSTATE_ACK;
 		} else {
-			TM_HD44780_Puts(0, SECOND_ROW, "Init state!");
-			Delayms(200);
+			lcdPutStrSecRow("Init state!");
+			PCIptr->usbState = USB_SUBSTATE_INIT;
 		}
 		break;
+
 	case USB_SUBSTATE_ACK:
-		TM_HD44780_Puts(0, SECOND_ROW, PCIptr->dataToSend);
+		lcdPutStrSecRow((char*)PCIptr->dataToSend);
 
-		message_length = sprintf(PCIptr->dataToSend, "provide_set_param\r\n");
+		message_length = sprintf((char*)PCIptr->dataToSend, "provide_set_param\r\n");
 		res = CDC_Transmit_FS(PCIptr->dataToSend, message_length);
 
-		if (res != USBD_OK){
-			TM_HD44780_Puts(0, SECOND_ROW, "Couldn't send message!");
-			Delayms(200);
-		}else {
-			TM_HD44780_Puts(0, SECOND_ROW, "Nxt-> CONF state!");
-			Delayms(200);
-			PCIptr->usbState = USB_SUBSTATE_CONF;
-			return PCIptr->usbState;
-		}
-		break;
-	case USB_SUBSTATE_CONF:
-		if (strstr(PCIptr->receivedData, "param")) {
-			PCIptr->usbState = USB_SUBSTATE_FINISHED;
-			return USB_SUBSTATE_FINISHED;
+		if (res != USBD_OK) {
+			lcdPutStrSecRow("Couldn't send message!");
+			PCIptr->usbState = USB_SUBSTATE_ERROR;
 		} else {
-			TM_HD44780_Puts(0, SECOND_ROW, "Unrecog. par. sent!");
-			Delayms(200);
+			lcdPutStrSecRow("Nxt-> CONF state!");
+			PCIptr->usbState = USB_SUBSTATE_CONF;
 		}
 		break;
+
+	case USB_SUBSTATE_CONF:
+		if (strstr((char*)PCIptr->receivedData, "param")) {
+			PCIptr->usbState = USB_SUBSTATE_FINISHED;
+		} else {
+			lcdPutStrSecRow("Unrecog. par. sent!");
+			PCIptr->usbState = USB_SUBSTATE_ERROR;
+		}
+		break;
+
 	case USB_SUBSTATE_FINISHED:
-		message_length = sprintf(PCIptr->dataToSend, "configure done!\r\n");
+		message_length = sprintf((char*)PCIptr->dataToSend, "configure done!\r\n");
 		res = CDC_Transmit_FS(PCIptr->dataToSend, message_length);
 
-		if (res != USBD_OK){
-					TM_HD44780_Puts(0, SECOND_ROW, "Couldn't send message!");
-					Delayms(200);
-				}else {
-					TM_HD44780_Puts(0, SECOND_ROW, "Nxt-> INIT state!");
-					Delayms(200);
-					PCIptr->usbState = USB_SUBSTATE_INIT;
-					return USB_SUBSTATE_INIT;
-				}
+		if (res != USBD_OK) {
+			lcdPutStrSecRow("Couldn't send message!");
+			PCIptr->usbState = USB_SUBSTATE_ERROR;
+		} else {
+			lcdPutStrSecRow("Nxt-> INIT state!");
+			PCIptr->usbState = USB_SUBSTATE_INIT;
+		}
 		break;
-	default :
-		TM_HD44780_Puts(0, SECOND_ROW, "Unknown state!");
-		Delayms(200);
-		return USB_SUBSTATE_ERROR;
+
+	case USB_SUBSTATE_ERROR:
+		lcdPutStrSecRow("Error!!!");
+		lcdPutStrSecRow("Nxt -> Init state");
+
+		PCIptr->usbState = USB_SUBSTATE_INIT;
+		break;
+	default:
+		lcdPutStrSecRow("Unknown state!");
+		PCIptr->usbState = USB_SUBSTATE_ERROR;
 		break;
 	}
+	return PCIptr->usbState;
 }
 
